da93639b6685a9a9bf92816529e34c72bc72b13f
Pai026 <abhirampai1999@gmail.com>
2021-01-01 20:19:40 +0530

    added sidebar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 const Material = [
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatSidenavModule,
+  MatIconModule,
 ]
 
 @NgModule({
